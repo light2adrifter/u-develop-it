961b9be104cd25ddea8b5426bf3f5f2f8a6f40d3
light <light2adrifter@yahoo.com>
2022-03-20 20:08:54 -0400

    added server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Default response for any other request (Not Found) make last route
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+});
